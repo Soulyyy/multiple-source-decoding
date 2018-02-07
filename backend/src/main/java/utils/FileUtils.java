0815d2d75bc72d077435bc0345e5421fca71fbdd
soulyyy@gmail.com <soulyyy@gmail.com>
2018-02-07 22:48:24 +0200

    #3 removed dead code

--- backend/src/main/java/utils/FileUtils.java
+++ backend/src/main/java/utils/FileUtils.java
@@ -1,16 +1,11 @@
 package utils;
 
-import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class FileUtils {
-
-  private static final Logger log = LoggerFactory.getLogger("Util");
 
   public static Stream<String> getResourceAsStream(String name) {
     try {
